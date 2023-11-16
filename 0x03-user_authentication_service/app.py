54f20c7068841d4da48cc8d691926b471a725778
Stevetech1 <steveojata54@gmail.com>
2023-11-16 13:13:35 -0800

    updated

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,112 +1,78 @@
 #!/usr/bin/env python3
 """
-API Route Module
+Route module for the API
 """
+from os import getenv
+from api.v1.views import app_views
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
 
-from flask import Flask, abort, jsonify, redirect, request
-from auth import Auth
 
-AUTH = Auth()
 app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+if getenv("AUTH_TYPE") == "auth":
+    auth = Auth()
+elif getenv("AUTH_TYPE") == "basic_auth":
+    auth = BasicAuth()
+elif getenv("AUTH_TYPE") == "session_auth":
+    auth = SessionAuth()
+elif getenv("AUTH_TYPE") == "session_exp_auth":
+    auth = SessionExpAuth()
+elif getenv("AUTH_TYPE") == "session_db_auth":
+    auth = SessionDBAuth()
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def index() -> str:
-    """returns a JSON payload"""
-    return jsonify({"message": "Bienvenue"})
-
-
-@app.route('/users', methods=['POST'], strict_slashes=False)
-def users() -> str:
-    """this method registers new users -> POST /users"""
-    email = request.form.get("email")
-    password = request.form.get("password")
-
-    try:
-        new_user = AUTH.register_user(email, password)
-        if new_user is not None:
-            return jsonify({
-                "email": f"{email}",
-                "message": "user created"
-            }), 200
-    except Exception:
-        return jsonify({
-            "message": "email already registered"
-        }), 400
-
-
-@app.route('/sessions', methods=['POST'], strict_slashes=False)
-def login() -> str:
-    """this function creates a new session for the user, store it in the
-    session ID as a cookie with key "session_id" and returns JSON payload"""
-    email = request.form.get('email')
-    password = request.form.get('password')
-    valid_login = AUTH.valid_login(email, password)
-
-    if not valid_login:
-        abort(401)
-    session_id = AUTH.create_session(email)
-    response = jsonify({"email": f"{email}", "message": "logged in"})
-    response.set_cookie('session_id', session_id)
-    return response
-
-
-@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
-def logout() -> str:
-    """this method finds the user with requested session ID, destroys the
-    session and redirect the user to GET /"""
-    session_id = request.cookies.get('session_id')
-    user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        AUTH.destroy_session(user.id)
-        return redirect('/')
-    else:
-        abort(403)
-
-
-@app.route('/profile', methods=['GET'], strict_slashes=False)
-def profile() -> str:
-    """this method gets user by using the session_id"""
-    session_id = request.cookies.get('session_id')
-    user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        return jsonify({"email": user.email}), 200
-    else:
-        abort(403)
-
-
-@app.route('/reset_password', methods=['POST'], strict_slashes=False)
-def get_reset_password_token() -> str:
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
     """
-    this function checks if provided email is registered
-    generates a token and respond with status 200
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
     """
-    email = request.form.get('email')
-    is_registered = AUTH.create_session(email)
-
-    if is_registered:
-        reset_token = AUTH.get_reset_password_token(email)
-        return jsonify({"email": email, "reset_token": reset_token})
-    else:
-        abort(403)
-
-
-@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
-def update_password() -> str:
-    """ PUT /reset_password
-      Return:
-        - JSOM payload message
+    Unauthorized handler.
     """
-    email = request.form.get('email')
-    reset_token = request.form.get('reset_token')
-    new_password = request.form.get('new_password')
-    try:
-        AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": f"{email}",
-                        "message": "Password updated"}), 200
-    except Exception:
-        abort(403)
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def unauthorized(error) -> str:
+    """
+    Forbidden handler.
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """
+    handler before_request
+    """
+    authorized_list = ['/api/v1/status/',
+                       '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                       '/api/v1/auth_session/login/']
+
+    if auth and auth.require_auth(request.path, authorized_list):
+        if auth.authorization_header(request) is None and \
+                auth.session_cookie(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
+        request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
